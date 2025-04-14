634c7cc088e61f031f4d7780fdf13ddd5afae3dd
BKCODE2003 <bhushankor12345.9@gmail.com>
2025-04-14 14:48:06 +0530

    Updated PWA build files 22

--- taskverse/build/web/flutter_service_worker.js
+++ taskverse/build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "cbbcdd64b76f1ad017f9248dbb265392
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "4c9ed99649c38e49598026f78d4e4653",
+"flutter_bootstrap.js": "d7611a5b6173e6773a8298a1a379d81f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
